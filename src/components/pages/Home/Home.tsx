c9eb13b3420de56b9c7ca1687f7eadaafe608558
outpac_ <imronbusy@gmail.com>
2026-01-31 04:33:18 +0500

    done all ui

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
 import { Products } from "./components"
+import usdtIcon from '/icons/usdt.svg'
+
+import styles from './Home.module.css'
+import { Button } from "../../shared/ui"
 
 export const Home = () => {
     return (
         <div className="container">
-            <Products />
+            <div className={styles.header}>
+                <h1 className={styles.header__title}>Mailzy</h1>
+                <div className={styles.header__balance}>
+                    <span>Balance: </span>
+                    <div className={styles.header__balance_info}>
+                        <img src={usdtIcon} alt="USDT" />
+                        <b>650$</b>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <Products />
+            </div>
+            <div className={styles.button}>
+                <Button >Buy Now {'15$'}</Button>
+            </div>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/reset.css'
+import './styles/index.css'
+import './styles/variables.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
