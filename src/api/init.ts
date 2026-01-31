07082d41d7cb648ec61f4fc26c526da36cb83266
outpac_ <imronbusy@gmail.com>
2026-01-31 10:49:10 +0500

    fix

--- src/api/init.ts
+++ src/api/init.ts
@@ -2,13 +2,19 @@ import { initData } from "../lib"
 import type { InitResponse } from "./api"
 
 export async function init(): Promise<InitResponse> {
+    const tg = window.Telegram?.WebApp ?? {
+  initData: '',
+  ready: () => {},
+  close: () => {},
+  expand: () => {}
+}
   const res = await fetch('http://localhost:3000/api/init', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      initData: initData
+      initData: tg.initData
     })
   })
 
