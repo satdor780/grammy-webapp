afe3542fc8fe955b2c6bcd12ec462e1914f535e8
outpac_ <imronbusy@gmail.com>
2026-02-05 21:17:15 +0500

    fix deploy errors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,5 @@
 export { api } from "./axios";
 export { init } from "./init";
 export { uploadImage } from "./uploadImage";
-export { createProduct } from "./createProduct";
 export * from "./types";
 export * from "./uploadImage";
-export * from "./createProduct";
