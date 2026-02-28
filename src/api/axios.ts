9131dc47ee21c07ebde94be1daaeab1d1d7be973
outpac_ <imronbusy@gmail.com>
2026-02-28 07:55:11 +0500

    add server url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+const API_BASE = import.meta.env.VITE_SERVER_URI!;
 
 export const api = axios.create({
   baseURL: API_BASE,
